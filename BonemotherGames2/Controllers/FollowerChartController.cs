1912adc78d6a284f519bc38c4f66667ad7ddfab1
Zachary Velcoff <zackfactual@gmail.com>
2020-11-03 16:38:58 -0600

    Add FollowerChartController:

---
+++ BonemotherGames2/Controllers/FollowerChartController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BonemotherGames.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BonemotherGames2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FollowerChartController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<FollowerChart> Get()
+        {
+            using (var ctx = new BonemotherGamesContext())
+            {
+                var followerChart = ctx.FollowerChart.ToList();
+                return followerChart;
+            }
+        }
+
+        [HttpGet("{leaderClassId}")]
+        public IEnumerable<FollowerChart> Get(int leaderClassId)
+        {
+            using (var ctx = new BonemotherGamesContext())
+            {
+                var followerMaps = ctx.FollowerSourceFollowerChart.Where(x => x.LeaderClassId == leaderClassId).ToList();
+                var followerCharts = new List<FollowerChart>();
+                foreach (var followerMap in followerMaps)
+                {
+                    var follower = ctx.FollowerChart.Where(x => x.FollowerChartId == followerMap.FollowerChartId).First();
+                    followerCharts.Add(follower);
+                }
+                return followerCharts;
+            }
+        }
+    }
+}
--- BonemotherGames2/Startup.cs
+++ BonemotherGames2/Startup.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using VueCliMiddleware;
 
 namespace BonemotherGames
@@ -46,7 +40,7 @@ namespace BonemotherGames
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllers(); 
             });
 
             app.UseSpa(spa =>

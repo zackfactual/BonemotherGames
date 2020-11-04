108596120a21ed6c8df6c9c107f823b4bda65aad
Zachary <zackfactual@gmail.com>
2020-11-04 14:57:29 -0600

    Add FollowerController and Follower class to allow construction of rollable charts based on follower source (aka leaderClassId):

---
+++ BonemotherGames2/Controllers/FollowerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using BonemotherGames2.Entities;
+using BonemotherGames2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BonemotherGames2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FollowerController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            List<Follower> followers = new List<Follower>();
+            using (var ctx = new BonemotherGamesContext())
+            {
+                var followerCharts = ctx.FollowerChart.ToList();
+                foreach (var followerChart in followerCharts)
+                {
+                    var follower = new Follower();
+                    follower.ConstructFollowerFromFollowerChart(follower, followerChart);
+                    followers.Add(follower);
+                }
+            }
+            return JsonSerializer.Serialize(followers);
+        }
+
+
+        [HttpGet("{leaderClassId}")]
+        public string Get(int leaderClassId)
+        {
+            List<Follower> followers = new List<Follower>();
+            using (var ctx = new BonemotherGamesContext())
+            {
+                var followerSourceFollowerCharts = ctx.FollowerSourceFollowerChart.Where(x => x.LeaderClassId == leaderClassId).ToList();
+                foreach (var followerSourceFollowerChart in followerSourceFollowerCharts)
+                {
+                    var follower = new Follower();
+                    follower.ConstructFollowerFromLeaderClassId(follower, followerSourceFollowerChart);
+                    followers.Add(follower);
+                }
+                return JsonSerializer.Serialize(followers);
+            }
+        }
+    }
+}
---
+++ BonemotherGames2/Services/Follower.cs
@@ -0,0 +1,58 @@
+﻿using BonemotherGames2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+
+namespace BonemotherGames2.Services
+{
+    public class Follower
+    {
+        public FollowerChart FollowerChart { get; set; }
+        public int LeaderClassId { get; set; }
+        public int LowRoll { get; set; }
+        public int HighRoll { get; set; }
+
+        public Follower()
+        {
+
+        }
+
+        public Follower(FollowerChart followerChart,
+            int leaderClassId,
+            int lowRoll,
+            int highRoll)
+        {
+            FollowerChart = followerChart;
+            LeaderClassId = leaderClassId;
+            LowRoll = lowRoll;
+            HighRoll = highRoll;
+        }
+
+        public Follower ConstructFollowerFromFollowerChart(Follower follower, FollowerChart followerChart)
+        {
+            follower.FollowerChart = followerChart;
+            using (var ctx = new BonemotherGamesContext())
+            {
+                var followerSourceFollowerChart = ctx.FollowerSourceFollowerChart.Where(x => x.FollowerChartId == follower.FollowerChart.FollowerChartId).First();
+                follower.LeaderClassId = followerSourceFollowerChart.LeaderClassId;
+                follower.LowRoll = followerSourceFollowerChart.LowRoll;
+                follower.HighRoll = followerSourceFollowerChart.HighRoll;
+            }
+            return follower;
+        }
+
+        public Follower ConstructFollowerFromLeaderClassId(Follower follower, FollowerSourceFollowerChart followerSourceFollowerChart)
+        {
+            using (var ctx = new BonemotherGamesContext())
+            {
+                follower.FollowerChart = ctx.FollowerChart.Where(x => x.FollowerChartId == followerSourceFollowerChart.FollowerChartId).First();
+                follower.LeaderClassId = followerSourceFollowerChart.LeaderClassId;
+                follower.LowRoll = followerSourceFollowerChart.LowRoll;
+                follower.HighRoll = followerSourceFollowerChart.HighRoll;
+            }
+            return follower;
+        }
+    }
+}

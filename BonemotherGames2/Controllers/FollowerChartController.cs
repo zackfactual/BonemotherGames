c0a88811925904bd9fa7c13a4de9f2a814be61d3
Elena <eondich@gmail.com>
2020-11-03 20:05:48 -0600

    Getting data from controller to retainer page

--- BonemotherGames2/Controllers/FollowerChartController.cs
+++ BonemotherGames2/Controllers/FollowerChartController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using BonemotherGames2.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,12 +12,12 @@ namespace BonemotherGames2.Controllers
     public class FollowerChartController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<FollowerChart> Get()
+        public string Get()
         {
             using (var ctx = new BonemotherGamesContext())
             {
                 var followerChart = ctx.FollowerChart.ToList();
-                return followerChart;
+                return JsonSerializer.Serialize(followerChart);
             }
         }
 
--- BonemotherGames2/clientapp/src/components/retainer/RetainerPage.vue
+++ BonemotherGames2/clientapp/src/components/retainer/RetainerPage.vue
@@ -5,9 +5,22 @@
 <script>
 import RetainerCard from '../retainer/RetainerCard.vue'
 
+import axios from 'axios'
+
 export default {
     components: {
         RetainerCard
+        },
+        data () {
+            return {
+                temp: null
+            }
+        },
+    created () {
+        axios.get('FollowerChart')
+            .then(result => {
+                this.temp = result
+            })
     }
 }
 </script>

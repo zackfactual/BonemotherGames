b9a4eac76d56e6ad5491e7a5611b8402fdfece15
Elena <eondich@gmail.com>
2020-11-18 17:51:05 -0600

    Using generic select component for retainer ancestry

--- BonemotherGames2/clientapp/src/components/retainer/RetainerCardEditor.vue
+++ BonemotherGames2/clientapp/src/components/retainer/RetainerCardEditor.vue
@@ -1,18 +1,24 @@
 ﻿<template>
     <div class="retainer-card-editor">
         <input type="text" name="retainerName" v-model="editedRetainer.Name">
-        <select v-if="availableSubancestries[selectedAncestryId]" name="retainerSubancestry" v-model="selectedSubancestryId" @change="selectSubancestry">
-            <option value="null">Subancestry</option>
-            <option v-for="subancestry in availableSubancestries[selectedAncestryId]"
-                    :key="`subancestry-${subancestry.SubancestryId}`"
-                    :value="subancestry.SubancestryId">{{ subancestry.SubancestryName }}</option>
-        </select>
-        <select name="retainerAncestry" v-model="selectedAncestryId" @change="selectAncestry($event)">
-            <option value="null">Ancestry</option>
-            <option v-for="ancestry in availableAncestries"
-                    :key="`ancestry-${ancestry.AncestryId}`"
-                    :value="ancestry.AncestryId">{{ ancestry.AncestryName }}</option>
-        </select>
+        <generic-select v-if="Object.keys(availableSubancestries[selectedAncestryId]).length"
+                        :default-value="defaultSubancestry"
+                        :default-enabled="!editedRetainer.Ancestry.SubancestryRequired"
+                        :option-object="availableSubancestries[selectedAncestryId]"
+                        option-key-selector="SubancestryId"
+                        option-text-selector="SubancestryName"
+                        option-value-selector="SubancestryId"
+                        name="retainerSubancestry"
+                        @selected="selectSubancestry($event)">
+            Subancestry
+        </generic-select>
+        <generic-select name="retainerAncestry"
+                        :default-value="defaultAncestry"
+                        :option-object="availableAncestries"
+                        option-key-selector="AncestryId"
+                        option-text-selector="AncestryName"
+                        option-value-selector="AncestryId"
+                        @selected="selectAncestry($event)">Ancestry</generic-select>
         <h3 v-if="editedRetainer">
             {{ editedRetainer.RetainerClass.ClassName }}
         </h3>
@@ -21,10 +27,14 @@
 
 <script>
 import CardMixin from '../mixins/CardMixin.js'
+import GenericSelect from '../shared/GenericSelect.vue'
 
 import axios from 'axios'
 
 export default {
+    components: {
+        GenericSelect
+    },
     mixins: [
         CardMixin
     ],
@@ -40,33 +50,44 @@ export default {
     },
     data () {
         return {
-            availableAncestries: null,
+            availableAncestries: {},
+            ancestryOptions: [],
             availableSubancestries: {},
             editedRetainer: null,
-            selectedAncestryId: null,
-            selectedSubancestryId: null
+            loading: false,
+            selectedClassId: null
+        }
+    },
+    computed: {
+        defaultAncestry () {
+            return this.editedRetainer.Ancestry.AncestryId
+        },
+        defaultSubancestry () {
+            return this.editedRetainer.Subancestry ? this.editedRetainer.Subancestry.SubancestryId : -1
+        },
+        selectedAncestryId () {
+            return this.editedRetainer.Ancestry.AncestryId
         }
     },
     beforeMount () {
         this.editedRetainer = this.retainer
-        if (this.editedRetainer.Subancestry) {
-            this.selectedSubancestryId = this.editedRetainer.Subancestry.SubancestryId
-        }
-        this.selectedAncestryId = this.editedRetainer.Ancestry.AncestryId
         this.getAvailableAncestries()
         this.getAvailableSubancestries(false)
     },
     methods: {
         getAvailableAncestries() {
+            this.loading = true
             axios.get('/ancestry')
                 .then(result => {
                     this.availableAncestries = result.data.reduce((ancestries, ancestry) => {
                         ancestries[ancestry.AncestryId] = ancestry
                         return ancestries
                     }, {})
+                    this.loading = false
                 })
         },
         getAvailableSubancestries (resetSubancestry) {
+            this.loading = true
             const ancestryId = this.editedRetainer.Ancestry.AncestryId
             if (!this.availableSubancestries[ancestryId]) {
                 axios.get(`/subancestry/${ancestryId}`)
@@ -79,8 +100,10 @@ export default {
                         if (resetSubancestry) {
                             this.resetSubancestry()
                         }
+                        this.loading = false
                     })
             } else {
+                this.loading = false
                 if (resetSubancestry) {
                     this.resetSubancestry()
                 }
@@ -89,23 +112,19 @@ export default {
         resetSubancestry () {
             if (this.editedRetainer.Ancestry.SubancestryRequired) {
                 const subancestryKeys = Object.keys(this.availableSubancestries[this.selectedAncestryId])
-                this.selectedSubancestryId = subancestryKeys[0]
-                this.$set(this.editedRetainer, 'Subancestry', this.availableSubancestries[this.selectedAncestryId][this.selectedSubancestryId])
+                this.$set(this.editedRetainer, 'Subancestry', this.availableSubancestries[this.selectedAncestryId][subancestryKeys[0]])
             } else {
-                this.selectedSubancestryId = null
                 this.$set(this.editedRetainer, 'Subancestry', null)
             }
         },
-        selectAncestry () {
-            const redirectQuery = { followerName: this.editedRetainer.Name, ancestry: this.selectedAncestryId }
+        selectAncestry (ancestryId) {
+            const redirectQuery = { followerName: this.editedRetainer.Name, ancestry: ancestryId }
             this.$router.push({ path: `/retainer_card/${this.editedRetainer.RetainerClass.RetainerClassId}`, query: redirectQuery })
-            this.$set(this.editedRetainer, 'Ancestry', this.availableAncestries[this.selectedAncestryId])
-            this.selectedSubancestryId = null
-            this.$set(this.editedRetainer, 'Subancestry', null)
+            this.$set(this.editedRetainer, 'Ancestry', this.availableAncestries[ancestryId])
             this.getAvailableSubancestries(true)
         },
-        selectSubancestry () {
-            const selectedSubancestry = this.availableSubancestries[this.selectedAncestryId][this.selectedSubancestryId]
+        selectSubancestry(subancestryId) {
+            const selectedSubancestry = this.availableSubancestries[this.selectedAncestryId][subancestryId]
             this.$set(this.editedRetainer, 'Subancestry', selectedSubancestry)
         }
     }
---
+++ BonemotherGames2/clientapp/src/components/shared/GenericSelect.vue
@@ -0,0 +1,70 @@
+﻿<template>
+    <select v-bind="{ name }" v-model="selectedOption" @change="selectOption">
+        <option v-if="defaultEnabled" value="-1"><slot /></option>
+        <option v-else value="-1"><slot /></option>
+        <option v-for="option in options"
+                :key="`option-${option[optionKeySelector]}`"
+                :value="option[optionValueSelector]">
+            {{ option[optionTextSelector] }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    props: {
+        defaultEnabled: {
+            type: Boolean,
+            default: false
+        },
+        defaultValue: {
+            type: Number,
+            default: -1
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        optionArray: {
+            type: Array,
+            default: () => []
+        },
+        optionObject: {
+            type: Object,
+            default: () => {}
+        },
+        optionKeySelector: {
+            type: String,
+            default: 'id'
+        },
+        optionTextSelector: {
+            type: String,
+            default: 'text'
+        },
+        optionValueSelector: {
+            type: String,
+            default: 'value'
+        }
+    },
+    computed: {
+        options () {
+            return this.optionArray.length ? this.optionArray : this.optionObject
+        }
+    },
+    data () {
+        return {
+            selectedOption: this.defaultValue
+        }
+    },
+    watch: {
+        defaultValue () {
+            this.selectedOption = this.defaultValue
+        }
+    },
+    methods: {
+        selectOption () {
+            this.$emit('selected', this.selectedOption)
+        }
+    }
+}
+</script>

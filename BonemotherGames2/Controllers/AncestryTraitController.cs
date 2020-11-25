dc579b15f36d8d8b15958857c5f793e2c8a33e7b
Zachary <zackfactual@gmail.com>
2020-11-24 20:05:42 -0600

    Create AncestryTraitController, IntegerExtensions:

---
+++ BonemotherGames2/Controllers/AncestryTraitController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BonemotherGames2.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BonemotherGames2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AncestryTraitController : ControllerBase
+    {
+        [HttpGet("{ancestryId}")]
+        public string Get(int ancestryId)
+        {
+            List<RetainerTrait> retainerTraits = new List<RetainerTrait>();
+            using (var ctx = new BonemotherGamesContext())
+            {
+                var ancestralRetainerTraits = ctx.AncestryRetainerTrait.Where(x => x.AncestryId == ancestryId);
+                foreach (var ancestralRetainerTrait in ancestralRetainerTraits)
+                {
+                    var trait = ctx.RetainerTrait.Where(x => x.TraitId == ancestralRetainerTrait.TraitId).First();
+                    retainerTraits.Add(trait);
+                }
+            }
+            var json = JsonConvert.SerializeObject(retainerTraits, new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+            return json;
+        }
+    }
+}
---
+++ BonemotherGames2/Services/IntegerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BonemotherGames2.Services
+{
+    public static class IntegerExtensions
+    {
+        public static string DisplayWithSuffix(this int num)
+        {
+            string number = num.ToString();
+            if (number.EndsWith("11")) return number + "th";
+            if (number.EndsWith("12")) return number + "th";
+            if (number.EndsWith("13")) return number + "th";
+            if (number.EndsWith("1")) return number + "st";
+            if (number.EndsWith("2")) return number + "nd";
+            if (number.EndsWith("3")) return number + "rd";
+            return number + "th";
+        }
+    }
+}
